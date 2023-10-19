99fb13c9e3a8fb975de4f96c06626a71bacef7cc
ador <rashedulhaque111@gmail.com>
2023-10-19 23:31:47 +0600

    MongoDB DataBase Updated

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o3wf1eu.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://restaurant:F4sVPZWRrSFnfW7E@cluster0.o3wf1eu.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -40,7 +41,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Delhi Restaurant Server is RUNNING');
+    res.send('Delhi Restaurant Server is RUNNING')
 })
 
 app.listen(port, () => {
